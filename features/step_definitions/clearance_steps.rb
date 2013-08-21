17ab52e13a8c30436199c60e0d9ef4877274e2f1
Nikolay Burlov <kohgpat@gmail.com>
2013-08-21 11:40:26 +0900

    Move sign in/up steps into clearance steps

--- features/step_definitions/clearance_steps.rb
+++ features/step_definitions/clearance_steps.rb
@@ -1,3 +1,16 @@
+When /^I go to the sign in page$/ do
+  visit sign_in_path
+end
+
+When /^I sign in as "(.*?)"$/ do |email|
+  steps %{
+    When I go to the sign in page
+    And I fill in "Email" with "#{email}"
+    And I fill in "Password" with "password"
+    And I press "Sign in"
+  }
+end
+
 When /^I go to the sign up page$/ do
   visit sign_up_path
 end
